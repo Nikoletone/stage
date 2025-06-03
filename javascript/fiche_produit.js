4843b3cfa0017bcb5c855d33a9fc5b30e722453b
Nikoletone <64850015+Nikoletone@users.noreply.github.com>
2025-06-03 17:15:43 +0200

    premier commit

---
+++ javascript/fiche_produit.js
@@ -0,0 +1,43 @@
+const popup = document.getElementById('productPopup');
+const popupTitle = document.getElementById('popupTitle');
+const popupImage = document.getElementById('popupImage');
+const popupPrice = document.getElementById('popupPrice');
+const popupDescription = document.getElementById('popupDescription');
+const popupDimensions = document.getElementById('popupDimensions');
+const closeBtn = document.querySelector('.close-btn');
+
+// On sélectionne TOUS les boutons
+const openButtons = document.querySelectorAll('.openPopupBtn');
+
+// Pour chaque bouton, on ajoute un clic
+openButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const productCard = button.closest('.product-card'); // remonte à la div parent
+    const name = productCard.dataset.name;
+    const price = productCard.dataset.price;
+    const description = productCard.dataset.description;
+    const image = productCard.dataset.image;
+    const dimensions = productCard.dataset.dimensions;
+
+    // Mise à jour du contenu de la popup
+    popupTitle.textContent = name;
+    popupPrice.textContent = price;
+    popupDescription.textContent = description;
+    popupImage.src = image;
+    popupDimensions.textContent = dimensions;
+
+    popup.style.display = 'flex';
+  });
+});
+
+// Fermer la popup
+closeBtn.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
+
+// Fermer en cliquant en dehors
+window.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    popup.style.display = 'none';
+  }
+});
---
+++ javascript/javascript_menu_burger.js
@@ -0,0 +1,10 @@
+const humburgerButton = document.querySelector(".nav-toggler");
+
+const navigation = document.querySelector("nav");
+
+humburgerButton.addEventListener("click", toggleNav)
+function toggleNav(){
+    humburgerButton.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
+
